1d46b758584bac5800a1e44ba74540e3ac89dcbf
Jason Young <thinkbigthings@gmail.com>
2024-05-15 12:40:51 -0400

    add error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,18 +1,23 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // A function whose name starts with a capital letter can be called by a function not in the same package.
 // This is known in Go as an exported name.
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 
-	// The := operator is a shortcut for declaring and initializing a variable in one line
-	// Go uses the value on the right to determine the variable's type
-	// So there is type inference, just like in Java
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("name parameter is empty but it should be a non-empty string")
+	}
 
-	// Return a greeting that embeds the name in a message.
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -8,7 +8,22 @@ import (
 // The main function is the entry point for the application.
 // run go run . in the hello directory
 func main() {
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Jason")
+	message, err := greetings.Hello("Jason")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Exiting")
+		return
+	}
+	fmt.Println(message)
+
+	// Get an error message and print it.
+	message, err = greetings.Hello("")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Exiting")
+		return
+	}
 	fmt.Println(message)
 }
